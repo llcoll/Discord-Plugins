1012194509297bb636e1591bbfb224c4697d2c42
llcoll <themasterperson@gmail.com>
2016-06-07 19:25:43 -0400

    more unhide attempts

--- plugins/messageHide.plugin.js
+++ plugins/messageHide.plugin.js
@@ -6,11 +6,11 @@ messageHide.prototype.start = function () {
 	$(document).on("mouseover.rpr", function(e) {
 		var contents = $('.chat>.content .messages .message-group .markup');
 		var hideBtn = '<span class="hide" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:10px;text-transform:uppercase;font-size:9px;padding:2px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Hide</span>';
-		var unhideBtn = '<span class="unhide" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:10px;text-transform:uppercase;font-size:9px;padding:2px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Unhide</span>';
+		var unhideBtn = '<span class="unhide" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:10px;text-transform:uppercase;font-size:9px;padding:2px 5px;visibility:hidden;box-sizing:border-box;background:rgba(0,0,0,0.4)">Unhide</span>';
 		contents.on('mouseover', function() {
+			$(unhideBtn).hide();
 			if($(this).find('.hide').length == 0) {
 				$(this).append(hideBtn);
-				$('unhideBtn').hide();
 				$(this).append(unhideBtn);
 				$('.hide').on('click', function() {
 					$(this).parent().toggle();
