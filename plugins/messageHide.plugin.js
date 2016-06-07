98a61997e9ed041432f41ed98e545a1c72571e58
llcoll <themasterperson@gmail.com>
2016-06-07 18:53:52 -0400

    got rid of var allChat

--- plugins/messageHide.plugin.js
+++ plugins/messageHide.plugin.js
@@ -4,20 +4,19 @@ var messageHide = function () {};
 
 messageHide.prototype.start = function () {
 	$(document).on("mouseover.rpr", function(e) {
-		var allChat = $('.messages .message-group');
 		var contents = $('.chat>.content .messages .message-group .markup');
-		var hideBtn = '<span class="hider" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:5px;text-transform:uppercase;font-size:10px;padding:3px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Hider</span>';
+		var hideBtn = '<span class="hide" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:5px;text-transform:uppercase;font-size:10px;padding:3px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Hide</span>';
 		contents.on('mouseover', function() {
-			if($(this).find('.hider').length == 0) {
+			if($(this).find('.hide').length == 0) {
 				$(this).append(hideBtn);
-				$('.hider').on('click', function() {
+				$('.hide').on('click', function() {
 					$(this).parent().toggle();
 				});
 			}
 		});
 		contents.on('mouseleave', function() {
-			if($(this).find('.hider').length == 1) {
-				$('.hider').empty().remove();
+			if($(this).find('.hide').length == 1) {
+				$('.hide').empty().remove();
 			}
 		});
 	});
