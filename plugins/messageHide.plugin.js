f4dfabe59dbe408dd5b9532d44287a48fe24d35a
Andrew <andrewjfilip@gmail.com>
2016-06-07 15:48:53 -0700

    Got it working, mostly

--- plugins/messageHide.plugin.js
+++ plugins/messageHide.plugin.js
@@ -3,25 +3,23 @@
 var messageHide = function () {};
 
 messageHide.prototype.start = function () {
-	$(document).on('mouseover', function() {
-		if(1 == 1) {
-			var allChat = $('.messages .message-group');
-			var contents = $('.chat>.content .messages .message-group .markup')
-			var hideBtn = '<span class="hide" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:5px;text-transform:uppercase;font-size:10px;padding:3px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Hide</span>';
-			allChat.on('mouseover', function() {
-				if(contents.find('.hide').length == 0) {
-					$(this).find(contents).append(hideBtn);
-					$('hide').on('click', function() {
-						$('contents').toggle();
-					});
-				}
-			});
-			allChat.on('mouseleave', function() {
-				if(contents.find('.hide').length == 1) {
-					$(this).find('.hide').empty().remove();
-				}
-			});
-		}
+	$(document).on("mouseover.rpr", function(e) {
+		var allChat = $('.messages .message-group');
+		var contents = $('.chat>.content .messages .message-group .markup');
+		var hideBtn = '<span class="hider" style="cursor:pointer;color:rgba(255,255,255,.8) !important;position:relative;top:-1px;margin-left:5px;text-transform:uppercase;font-size:10px;padding:3px 5px;box-sizing:border-box;background:rgba(0,0,0,0.4)">Hider</span>';
+		contents.on('mouseover', function() {
+			if($(this).find('.hider').length == 0) {
+				$(this).append(hideBtn);
+				$('.hider').on('click', function() {
+					$(this).parent().toggle();
+				});
+			}
+		});
+		contents.on('mouseleave', function() {
+			if($(this).find('.hider').length == 1) {
+				$('.hider').empty().remove();
+			}
+		});
 	});
 	console.log('~messageHide initiated~');
 };
@@ -29,11 +27,11 @@ messageHide.prototype.start = function () {
 messageHide.prototype.load = function () {};
 
 messageHide.prototype.unload = function () {
-	$('contents').show();
+	$(document).off("mouseover.rpr");
 };
 
 messageHide.prototype.stop = function () {
-	$('contents').show();
+	$(document).off("mouseover.rpr");
 };
 
 messageHide.prototype.onMessage = function () {
